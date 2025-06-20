ca63075da413f774f5c073f07b6bf50f39fbc6d0
gayatriakhila <152960007+gayatriakhila@users.noreply.github.com>
2025-06-20 17:37:12 +0530

    Add files via upload

---
+++ string functions 8-01.py
@@ -0,0 +1,81 @@
+#string in static
+'''name="Akhila"
+print(name)'''
+
+#string in Dynamic
+'''name=str(input("Enter name "))
+print(name)'''
+
+#Access 1st index element
+'''word = "Hello"
+print(word[1])'''
+
+#Slicing
+'''word = "Hello"
+print(word[1:4])'''
+
+#Multiline string
+'''message="""
+Honesty is best policy.
+Stay with honesty
+"""
+print(message)'''
+
+#compare 2 strings
+'''str1= "Hello World!"
+str2= "I Love Python"
+str3= "Hello World!"
+print(str1==str2)
+print(str1==str3)'''
+
+#Concatenate or Join strings
+'''str1= "Akhi"
+str2= "la"
+result=str1+str2
+print(result)
+#print(str1+str2)'''
+
+#Iterate through strings
+'''name="Bhavitha"
+for letter in name:
+    print(letter)'''
+
+#length of string
+'''name= "Sri Dharani"
+print(len(name))'''
+
+#lower to upper
+'''name="akhila"
+print(name.upper())'''
+
+'''firstString= "ideal"
+secondString= "IDEAL"
+if(firstString.upper == secondString.upper):
+    print("Both string are same")
+else:
+    print("Both string are not same")'''
+
+#upper to lower
+'''name="AKHILA"
+print(name.lower())'''
+
+'''firstString= "ideal"
+secondString= "IDEAL"
+if(firstString.upper == secondString.lower):
+    print("Both string are same")
+else:
+    print("Both string are not same")'''
+
+#String Partition
+'''name = "Ideal institute of technology"
+print(name.partition('of'))
+print(name.partition('not'))
+name = "Ideal institute of technology, kakinada"
+print(name.partition('institute'))'''
+
+#string replace
+'''text='Ramesh'
+replaced_text=text.replace('Ram','Raj')
+print(replaced_text)'''
+
+#
